b72e111c199bfe47f8b2b57c81e0b3a8bee54e96
ihsanlearn <iihsannlearn@gmail.com>
2025-12-24 16:11:07 +0700

    initial commit

--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,7 @@ import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   debug: true,
+  secret: process.env.AUTH_SECRET,
   providers: [
     GitHub({
         authorization: { params: { scope: "repo" } },
